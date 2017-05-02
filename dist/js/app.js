55e7c5f222f111f68f512893307eb253afd14623
Shannon Capper <shannonlcapper@gmail.com>
2017-05-01 17:01:17 -0700

    refactor nav-link to use ng-transclude

--- dist/js/app.js
+++ dist/js/app.js
@@ -204,11 +204,11 @@
       return {
         restrict: "E",
         scope: {
-          label: "@",
           goesTo: "@"
         },
+        transclude: true,
         //template needs <li> portion to make scrollspy work
-        template: "<li><a ng-href='{{goesTo}}' scroll-on-click collapse-on-click collapse-this='.navbar-collapse'>{{label}}</a></li>",
+        template: "<li><a ng-href='{{goesTo}}' scroll-on-click collapse-on-click collapse-this='.navbar-collapse'><ng-transclude></a></li>",
         link: function(scope, element, attrs, parentCtrl) {
         }
       }
