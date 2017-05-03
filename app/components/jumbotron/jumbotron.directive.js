dc91c3f898db33c03e34054d03ed43f2f30c44a6
Shannon Capper <shannonlcapper@gmail.com>
2017-05-03 00:08:09 -0700

    attempt to fix way zoomed in jumbotron bg using jquery.backgstretch

---
+++ app/components/jumbotron/jumbotron.directive.js
@@ -0,0 +1,19 @@
+"use strict";
+
+(function() {
+	
+	angular
+		.module("jumbotron")
+		.directive("jumbotron", function() {
+      return {
+        restrict: "E",
+        replace: true,
+        scope: {},
+        templateUrl: "app/components/jumbotron/jumbotron.html",
+        link: function(scope, element, attrs) {
+          element.backstretch();
+        }
+      };
+		});
+
+})();
