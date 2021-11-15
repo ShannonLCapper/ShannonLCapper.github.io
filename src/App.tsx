5202050ecac41d77532021ff2e1809deb5353603
Shannon Capper <shannon@textio.com>
2021-11-14 18:32:40 -0800

    Update favicon and manifest.json

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,10 @@ import 'src/App.css';
 
 import React from 'react';
 
-import logo from 'src/logo.svg';
-
 export const App = () => {
     return (
         <div className='App'>
             <header className='App-header'>
-                <img src={logo} className='App-logo' alt='logo' />
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
